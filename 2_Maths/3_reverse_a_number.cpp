ba6e239425199130bf85d9f233988f3194f0bbc0
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-09-25 05:15:49 +0530

    maths2

--- 2_Maths/3_reverse_a_number.cpp
+++ 2_Maths/3_reverse_a_number.cpp
@@ -3,13 +3,22 @@ using namespace std;
 
 
 int main(){
-    int n= 1234, newNo=0;
+    // int n= 1234, newNo=0;
 
-    while (n !=0 ){
-        newNo = newNo *10 + n%10;
+    // while (n !=0 ){
+    //     newNo = newNo *10 + n%10;
+    //     n/=10;
+    // }
+    
+    // cout<< newNo << endl;
+
+    int n= 1234;
+    int newNo=0;
+    while(n>=1){
+        newNo = newNo*10 + n%10;
         n/=10;
     }
-    
-    cout<< newNo << endl;
+    cout << newNo << endl;
 
 }
+
--- 2_Maths/4_print_all_divisor.cpp
+++ 2_Maths/4_print_all_divisor.cpp
@@ -2,18 +2,36 @@
 using namespace std;
 
 int main(){
-    int n =24;
-    vector <int> divisors;
-    for (int k =1 ; k*k<=n; k++){
-        if (n%k == 0){
-            divisors.push_back(k);
-            if (k != (n/k)) divisors.push_back(n/k);
+    // int n =24;
+    // vector <int> divisors;
+    // for (int k =1 ; k*k<=n; k++){
+    //     if (n%k == 0){
+    //         divisors.push_back(k);
+    //         if (k != (n/k)) divisors.push_back(n/k);
+    //     }
+    // }
+    // sort(divisors.begin(), divisors.end());
+    // for (auto i : divisors){
+    //     cout<<i<<" ";
+    // }
+
+    vector <int> v1;
+    int n = 10;
+    for (int i=1 ; i*i<=n ; i++ ){
+        if((n%i)==0){
+            v1.push_back(i);
+        }
+        if(i!=(n/i)){
+            v1.push_back(n/i);
         }
     }
-    sort(divisors.begin(), divisors.end());
-    for (auto i : divisors){
-        cout<<i<<" ";
+    sort(v1.begin(),v1.end());
+
+    for (auto i : v1){
+        cout << i << " ";
     }
+
+
     return 0;
     
 }
